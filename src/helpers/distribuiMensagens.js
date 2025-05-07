7f4cb50c002a3d5b615667e470f45ac2d7d92ae4
juliocesar249 <jc2493718@gmail.com>
2025-05-07 17:50:03 -0300

    resolvido: mensagens não sendo enviadas

--- src/helpers/distribuiMensagens.js
+++ src/helpers/distribuiMensagens.js
@@ -1,5 +1,5 @@
 export default async function distruiMensagens(conexao, mensagens) {
-    for(const mensagem in mensagens) {
+    for(const mensagem of mensagens) {
         await conexao.send(JSON.stringify(mensagem));
     }
 }
